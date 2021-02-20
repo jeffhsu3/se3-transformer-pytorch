b9f4bab0927a091af0498e60c9d33fe10473741a
jeffhsu3 <jeffhsu3@gmail.com>
2021-02-20 01:42:32 +0000

    test irrep comp with lie_learn

--- tests/test_irrep_repr.py
+++ tests/test_irrep_repr.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+from lie_learn.representations.SO3.pinchon_hoggan.pinchon_hoggan_dense import SO3_irrep
 from se3_transformer_pytorch.spherical_harmonics import clear_spherical_harmonics_cache
 from se3_transformer_pytorch.irr_repr import spherical_harmonics, irr_repr, compose
 from se3_transformer_pytorch.utils import torch_default_dtype
@@ -31,3 +33,18 @@ def test_irr_repr():
         d, r = (Yrx - DrY).abs().max(), Y.abs().max()
         print(d.item(), r.item())
         assert d < 1e-10 * r, d / r
+
+
+@torch_default_dtype(torch.float64)
+def test_irr_repr_lie_learn():
+    """ 
+    Test that lucidrain's implementation is roughly equivalent to lie_learn's SO3 
+    irr_rep implementation.
+    """
+
+    for order in range(7):
+        abg = torch.rand((3, 1))
+        ll_Dr = SO3_irrep(abg, order)
+        ll_Dr = np.squeeze(ll_Dr, axis=2)
+        lr_Dr = np.array(irr_repr(order, abg[0][0], abg[1][0], abg[2][0]))
+        np.testing.assert_allclose(ll_Dr, lr_Dr)
